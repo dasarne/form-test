0f5f5f63b5f1c02b12500b8d3efbc9651d73e372
dasarne <github@a-vi.de>
2016-12-11 20:57:14 +0100

    Form und Debug-Panel  hinzugefügt

--- src/app/entry/entry-form/entry-form.component.ts
+++ src/app/entry/entry-form/entry-form.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from "@angular/core";
+import {Entry} from "../shared";
 
 @Component({
   selector: 'ft-entry-form',
@@ -8,6 +9,19 @@ import {Component, OnInit} from "@angular/core";
 export class EntryFormComponent implements OnInit {
 
   constructor() {
+  }
+
+  //Instantiate the model
+  model = new Entry(1, "Arne", 50);
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+  }
+
+  // TODO: Remove this when we're done
+  get diagnostic() {
+    return JSON.stringify(this.model);
   }
 
   ngOnInit() {
--- src/app/entry/entry.module.ts
+++ src/app/entry/entry.module.ts
@@ -3,13 +3,16 @@ import {CommonModule} from "@angular/common";
 import {EntryComponent} from "./entry.component";
 import {EntryFormComponent} from "./entry-form/entry-form.component";
 import {routing} from "./entry.routing";
+import {FormsModule} from "@angular/forms";
+import {DebugPanelComponent} from "./debug-panel/debug-panel.component";
 
 @NgModule({
   imports: [
+    FormsModule,
     CommonModule,
     routing
   ],
-  declarations: [EntryComponent, EntryFormComponent]
+  declarations: [EntryComponent, EntryFormComponent, DebugPanelComponent]
 })
 export class EntryModule {
 }
